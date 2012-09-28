a636bb38de19b6dfb19e102f0ae276ca71827c37
Jenson Wu <wubofengwbf@gmail.com>
2012-09-28 09:28:36 +0800

    change

--- config/databases.php
+++ config/databases.php
@@ -4,110 +4,12 @@ $database = array(
 
     'master' => array(
 
-        'quan' => array(
-            'host' => '10.1.1.66',
-            'port' => 4306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'quan',
-            'char' => 'utf8',
-            ),
-
-        'forum' => array(
-            'host' => '10.1.1.1',
-            'port' => 4306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'gbb',
-            'char' => 'gbk',
-            ),
-
-        'attachment' => array(
-            'host' => '10.1.1.1',
-            'port' => 4306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'gbb',
-            'char' => 'gbk',
-            ),
-
-        'topic' => array(
-            'host' => '10.1.1.1',
-            'port' => 4306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'topic',
-            'char' => 'gbk',
-            ),
-
-        'reply' => array(
-            'host' => '10.1.1.63',
+        'test' => array(
+            'host' => '',
             'port' => 3306,
             'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'reply',
-            'char' => 'gbk',
-            ),
-
-        'user_63' => array(
-            'host' => '10.1.1.63',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'user',
-            'char' => 'gbk',
-            ),
-
-        'user' => array(
-            'host' => '10.1.94.200',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => '123456',
-            'name' => 'user',
-            'char' => 'gbk',
-            ),
-        'session' => array(
-            'host' => '10.1.1.17',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'session',
-            'char' => 'gbk',
-            ),
-
-        'message' => array(
-            'host' => '10.1.1.138',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'message',
-            'char' => 'gbk',
-            ),
-
-        'post' => array(
-            'host' => '10.1.1.17',
-            'port' => 4306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'reply',
-            'char' => 'gbk',
-            ),
-
-        'verify' => array(
-            'host' => '10.1.1.17',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'bbs_verify',
-            'char' => 'gbk',
-            ),
-
-        'home' => array(
-            'host' => '10.1.1.119',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'feeds',
+            'pass' => '',
+            'name' => '',
             'char' => 'utf8',
             ),
 
@@ -115,33 +17,6 @@ $database = array(
 
     'slave' => array(
 
-        'forum' => array(
-            'host' => '10.1.1.10',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'gbb',
-            'char' => 'gbk',
-            ),
-
-        'topic' => array(
-            'host' => '10.1.1.10',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'topic',
-            'char' => 'gbk',
-            ),
-
-        'reply' => array(
-            'host' => '10.1.1.75',
-            'port' => 3306,
-            'user' => 'root',
-            'pass' => 'Adfj89cc/*-+',
-            'name' => 'reply',
-            'char' => 'gbk',
-            ),
-        
         ),
     
     );
